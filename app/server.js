4ede5c85831302eb7a38274499eaba65e26c0c41
Peter Lyons <pete@peterlyons.com>
2014-01-21 11:34:38 -0700

    server port from coffeescript to javascript. That's the last commonjs module to port

---
+++ app/server.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+var child_process = require("child_process");
+var config = require("app/config");
+var express = require("express");
+var NotFound = require("./NotFound");
+var path = require("path");
+
+var app = express();
+app.set("view engine", "jade");
+app.set("views", __dirname);
+app.locals({
+  config: config,
+  appURI: config.appURI
+});
+app.use(express.logger({
+  immediate: true,
+  format: ":method :url :date"
+}));
+
+[
+  "blogs/blogRoutes",
+  "pages/pagesRoutes",
+  "photos/photosRoutes",
+  "photos/galleriesRoutes",
+  "site/cssRoutes",
+  "site/errorRoutes"
+].forEach(function(routesPath) {
+  require("app/" + routesPath)(app);
+});
+
+app.use(express.static(config.staticDir));
+app.use(function(req, res, next) {
+  next(new NotFound(req.path));
+});
+
+app.use(function(error, req, res, next) {
+  console.log("Error handler middleware:", error);
+  if (error instanceof NotFound) {
+    res.render("site/error404");
+  } else {
+    res.render("site/error500");
+  }
+});
+
+var ip = config.loopback ? "127.0.0.1" : "0.0.0.0";
+
+app.listen(config.port, ip, function(error) {
+  if (error) {
+    throw error;
+  }
+  console.log(
+    "Express serving on http://" + ip + ":" + config.port,
+    "\n\tbaseURL: " + config.baseURL,
+    "\n\tenv: " + process.env.NODE_ENV
+  );
+});
+
+if (config.inspector.enabled) {
+  child_process.fork(
+    require.resolve("node-inspector/bin/inspector"),
+    ["--web-port=" + config.inspector.webPort, "--web-host=127.0.0.1"],
+    {execArgv: []}
+  );
+}
