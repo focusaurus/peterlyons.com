ea421f5cdf98f57771d669894e812791fbcafbbe
Peter Lyons <pete@peterlyons.com>
2014-01-29 10:53:19 -0700

    added clue video and image to plusParty

--- app/plusParty/browser/plusParty.js
+++ app/plusParty/browser/plusParty.js
@@ -5,13 +5,18 @@
   function PlusPartyController($scope) {
     this.scope = $scope;
     this.scope.rawText = "Paste some numbers in here and we'll total them up" +
-      " even if there's some words and junk, too.";
+      " even if there's some words and junk, too.\n\n" +
+      "For example: 1 plus 2 plus 2 plus 1";
     this.scope.$watch("rawText", parseNumbers.bind(this));
     this.scope.numbers = [];
   }
 
-  function sum(subTotal, wrappedNumber) {
-    return subTotal + wrappedNumber.value;
+  function sum(subTotal, number) {
+    return subTotal + number;
+  }
+
+  function wrap(number) {
+    return {value: number};
   }
 
   function parseNumbers() {
@@ -19,8 +24,11 @@
     var match;
     while ((match = NUMBER_RE.exec(this.scope.rawText))) {
       var number = parseFloat(match[0].replace(COMMA_RE, ""), 10);
-      this.scope.numbers.push({value: number});
+      this.scope.numbers.push(number);
     }
+    //AngularJS repeaters require distinct objects, so we have to
+    //wrap our raw floats in objects to accommodate that
+    this.scope.wrappedNumbers = this.scope.numbers.map(wrap);
     this.scope.total = this.scope.numbers.reduce(sum, 0);
   }
 
