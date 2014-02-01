0a1d438181d9db80b7fe44c1ce124833da9a31f6
Peter Lyons <pete@peterlyons.com>
2014-02-01 07:24:09 -0700

    tests for plusParty.recompute

--- app/plusParty/browser/index.js
+++
@@ -1,1 +0,0 @@
-plusParty.js
--- test/browser/plusParty.js
+++ test/browser/plusParty.js
@@ -23,6 +23,7 @@ describe("plusParty", function () {
       assert.deepEqual(plusParty.parseNumbers("0 0 79"), [0, 0, 79]);
       assert.deepEqual(plusParty.parseNumbers("   \t\n26\t\r32\n"), [26, 32]);
     });
+
     it("should handle simple decimal points and negatives", function() {
       assert.deepEqual(plusParty.parseNumbers("-42"), [-42]);
       assert.deepEqual(plusParty.parseNumbers("42.15 666"), [42.15, 666]);
@@ -30,18 +31,33 @@ describe("plusParty", function () {
       assert.deepEqual(plusParty.parseNumbers(
         "   \t\n-26.987\t\r32.005\n"), [-26.987, 32.005]);
     });
+
     it("should handle currency symbols", function() {
       assert.deepEqual(plusParty.parseNumbers("$42"), [42]);
       assert.deepEqual(
         plusParty.parseNumbers("$42.15 $666 £92"), [42.15, 666, 92]);
       assert.deepEqual(plusParty.parseNumbers("€0"), [0]);
     });
+
     it("should ignore mm/dd/yyyy dates", function() {
       assert.deepEqual(plusParty.parseNumbers("12/31/1984"), []);
       assert.deepEqual(
         plusParty.parseNumbers("6 09/20/78 17"), [6, 17]);
     });
   });
+
+  describe("recompute", function () {
+    it("should convert rawText into numbers and total", function() {
+      var instance = {
+        scope: {
+          rawText: "I need 42 chickens and 7 mice"
+        }
+      };
+      plusParty.recompute.call(instance);
+      assert.deepEqual(instance.scope.numbers, [42, 7]);
+      assert.equal(instance.scope.wrappedNumbers[0].value, 42);
+      assert.equal(instance.scope.wrappedNumbers[1].value, 7);
+      assert.equal(instance.scope.total, 49);
+    });
+  });
 });
-
-
