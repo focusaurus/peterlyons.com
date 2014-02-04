2e261142aba09859c64e905cf351c17f29e519fd
Peter Lyons <pete@peterlyons.com>
2014-02-04 11:48:08 -0700

    slug tests assert to expectations

--- test/unit/slug.js
+++ test/unit/slug.js
@@ -1,22 +1,22 @@
-var assert = require("assert");
+var expectations = require("expectations");
 var slug = require("app/blogs/slug");
 
 describe("app/blogs/slug", function () {
   it("should replace URL-unfriendly characters", function() {
-    assert.equal(slug("space to dash"), "space-to-dash");
-    assert.equal(slug("no apostrophe's"), "no-apostrophes");
-    assert.equal(slug("dot.to.dash"), "dot-to-dash");
-    assert.equal(slug("double--dash--to--dash"), "double-dash-to-dash");
-    assert.equal(slug("no trailing dash-"), "no-trailing-dash");
-    assert.equal(slug("LOWERCASE"), "lowercase");
+    expect(slug("space to dash")).toBe("space-to-dash");
+    expect(slug("no apostrophe's")).toBe("no-apostrophes");
+    expect(slug("dot.to.dash")).toBe("dot-to-dash");
+    expect(slug("double--dash--to--dash")).toBe("double-dash-to-dash");
+    expect(slug("no trailing dash-")).toBe("no-trailing-dash");
+    expect(slug("LOWERCASE")).toBe("lowercase");
   });
 
   it("should normalize to lowercase", function() {
-    assert.equal(slug("LOWERCASE"), "lowercase");
+    expect(slug("LOWERCASE")).toBe("lowercase");
   });
 
   it("should handle null", function() {
-    assert.equal(slug(undefined), "");
-    assert.equal(slug(null), "");
+    expect(slug(undefined)).toBe("");
+    expect(slug(null)).toBe("");
   });
 });
