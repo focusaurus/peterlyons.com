9c5ed3d31224ce5c29ddbfe9809cb85cac7edb4e
Peter Lyons <pete@peterlyons.com>
2014-02-17 12:43:57 -0700

    mobile nav menu. Better media query approach

--- app/server.js
+++ app/server.js
@@ -72,5 +72,9 @@ if (config.inspector.enabled) {
       ["--web-port=" + config.inspector.webPort, "--web-host=127.0.0.1"],
       {execArgv: []}
     );
+    //tell node to start up the v8 debugger
+    //delay is here to avoid TCP port bind conflicts during node-dev restarts
+    process.kill(process.pid, "SIGUSR1");
+
   }, 2000);
 }
