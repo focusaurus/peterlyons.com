c6a537f61bc80b806af0a6f758d434cc156a8a2c
Peter Lyons <pete@peterlyons.com>
2014-02-22 20:34:15 -0700

    use upstart console log instead of multilog

--- deploy/ansible_prereqs.sh
+++ deploy/ansible_prereqs.sh
@@ -2,4 +2,4 @@
 #install ansible prereqs manually or all apt-based ansible commands will fail
 # http://euphonious-intuition.com/2013/01/bootstrapping-a-cluster-with-ansible-debian-6-and-oracle-java-7/
 apt-get update
-apt-get install -y python python-apt python-pycurl
+apt-get install --yes python python-apt python-pycurl
