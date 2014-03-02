ab2ad4cf3e06ae760025771741191722126c3a45
Peter Lyons <pete@peterlyons.com>
2014-03-02 15:16:06 -0700

    new career sort button code

--- app/browser/career.js
+++ app/browser/career.js
@@ -1,26 +1,28 @@
-function init() {
-  var button = document.querySelector(".reverse button");
-  var initialText = button.textContent;
+function sort(event) {
+  var activeButton = document.querySelector(".activeSort")
+  activeButton.classList.remove("activeSort");
+  activeButton.removeAttribute("disabled");
+
+  event.target.classList.add("activeSort");
+  event.target.setAttribute("disabled", "disabled");
   var nodeList = document.querySelectorAll(".phase");
   var lastPhase = nodeList[nodeList.length - 1];
   var parent = lastPhase.parentNode;
   var elementAfterPhases = lastPhase.nextSibling;
-  button.addEventListener("click", function() {
-    var phases = [];
-    [].forEach.call(document.querySelectorAll(".phase"), function(phase) {
-      phases.unshift(phase);
-    });
-    var fragment = document.createDocumentFragment();
-    phases.forEach(function(phase) {
-      fragment.appendChild(phase);
-    });
-    parent.insertBefore(fragment, elementAfterPhases);
-    if (button.textContent === initialText) {
-      button.textContent = "View newest first";
-    } else {
-      button.textContent = initialText;
-    }
+  var phases = [];
+  [].forEach.call(document.querySelectorAll(".phase"), function(phase) {
+    phases.unshift(phase);
   });
+  var fragment = document.createDocumentFragment();
+  phases.forEach(function(phase) {
+    fragment.appendChild(phase);
+  });
+  parent.insertBefore(fragment, elementAfterPhases);
+}
+
+function init() {
+  document.querySelector("button.newest").addEventListener("click", sort);
+  document.querySelector("button.chronological").addEventListener("click", sort);
 }
 
 module.exports = init;
