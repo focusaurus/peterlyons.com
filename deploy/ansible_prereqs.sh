c651a0a1cf273fc562742371575f276e4b0e45a4
Peter Lyons <pete@peterlyons.com>
2015-01-07 18:23:35 -0700

    rework ansible prereqs

--- deploy/ansible_prereqs.sh
+++ deploy/ansible_prereqs.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
-#install ansible prereqs manually or all apt-based ansible commands will fail
+#!/bin/bash
+# install ansible prereqs manually or all apt-based ansible commands will fail
+# http://stackoverflow.com/questions/22753081/bootstrapping-ansible-prerequisites-with-the-script-module-python-required/22763407#22763407
 # http://euphonious-intuition.com/2013/01/bootstrapping-a-cluster-with-ansible-debian-6-and-oracle-java-7/
-apt-get update
-apt-get install --yes python python-apt python-pycurl
+set -e
+apt-get -qq update
+apt-get -qq --yes install python python-apt python-pycurl sshpass
+touch /root/.ansible_prereqs_installed
