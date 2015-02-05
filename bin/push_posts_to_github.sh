617e162721f1f940a5d6d5f9195c97e9dcd03b73
Peter Lyons <pete@peterlyons.com>
2015-02-05 10:26:05 -0700

    need to specify git config for user/email

--- bin/push_posts_to_github.sh
+++ bin/push_posts_to_github.sh
@@ -2,6 +2,8 @@
 # Usage: push_posts_to_github.sh <token>
 set -e
 cd $(dirname "$0")/../../data
+git config --local "user.email" "pete@peterlyons.com"
+git config --local "user.name" "Peter Lyons"
 git add posts
 git commit -m "new blog post from web UI"
 git push "https://${1}:x-oauth-basic@github.com/focusaurus/data.git" master
