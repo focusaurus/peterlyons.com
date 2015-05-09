58018abd874d5c3599a1a38b668271cce8a7c7ac
Peter Lyons <pete@peterlyons.com>
2015-05-09 14:12:51 -0600

    fix build script to build reveal

--- bin/build_js.sh
+++ bin/build_js.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd $(dirname "$0")/..
 browserify $(./bin/browserify_args.sh) "$@" | uglifyjs --no-mangle > www/plws.js
+browserify -e app/browser/deck | uglifyjs --no-mangle > www/reveal.js
