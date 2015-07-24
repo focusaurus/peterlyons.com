5f53a6cdfdd1343938c67514b115f54bcd85a1fb
Peter Lyons <pete@peterlyons.com>
2015-07-24 19:07:36 -0400

    responsive design with rupture stylus plugin

--- app/site/cssRoutes.js
+++ app/site/cssRoutes.js
@@ -1,12 +1,17 @@
-var stylish = require("stylish");
+var autoprefixer = require("autoprefixer-stylus")
 var nib = require("nib");
+var rupture = require("rupture");
+var stylish = require("stylish");
 
 function setup(app) {
   app.use(stylish({
     src: __dirname,
     compress: true,
     setup: function(renderer) {
-      return renderer.use(nib());
+      return renderer
+        .use(nib())
+        .use(rupture())
+        .use(autoprefixer());
     }
   }));
 }
