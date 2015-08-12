4c869a08ff746c292d143eb9f52249753dd90656
Peter Lyons <pete@peterlyons.com>
2015-08-11 23:04:14 -0600

    add timestamp to blog commit messages

--- bin/push_posts_to_github.sh
+++ bin/push_posts_to_github.sh
@@ -5,5 +5,5 @@ cd $(dirname "$0")/../../data
 git config --local "user.email" "pete@peterlyons.com"
 git config --local "user.name" "Peter Lyons"
 git add posts
-git commit -m "new blog post from web UI"
+git commit -m "new blog post from web UI $(date)"
 git push "https://${1}:x-oauth-basic@github.com/focusaurus/data.git" master
