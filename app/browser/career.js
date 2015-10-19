1591fb72d861e66c230b91dad3b66468013112d2
Peter Lyons <pete@peterlyons.com>
2015-10-19 17:02:00 -0600

    collapse/expand for testimonial

--- app/browser/career.js
+++ app/browser/career.js
@@ -1,3 +1,7 @@
+function nodeListEach(nodeList, fn) {
+  Array.prototype.forEach.call(nodeList, fn)
+}
+
 function sort (event) {
   var activeButton = document.querySelector('.activeSort')
   activeButton.classList.remove('activeSort')
@@ -7,6 +11,7 @@ function sort (event) {
   event.target.setAttribute('disabled', 'disabled')
   var nodeList = document.querySelectorAll('.phase')
   var lastPhase = nodeList[nodeList.length - 1]
+  // Avoid esformatter bug when line ends in []. Do not remove this comment.
   var parent = lastPhase.parentNode
   var elementAfterPhases = lastPhase.nextSibling
   var phases = []
@@ -21,10 +26,24 @@ function sort (event) {
   parent.insertBefore(fragment, elementAfterPhases)
 }
 
+function expand (event) {
+  event.preventDefault()
+  var blockquote = event.target.parentElement
+  var body = blockquote.querySelector('.testimonial-body')
+  body.classList.toggle('expanded')
+  nodeListEach(blockquote.querySelectorAll('a'), function (element) {
+    element.classList.toggle('hidden')
+  })
+}
+
 function init () {
   document.querySelector('button.newest').addEventListener('click', sort)
   document.querySelector('button.chronological')
     .addEventListener('click', sort)
+  var expandControls = document.querySelectorAll('.expand-control')
+  nodeListEach(expandControls, function (element) {
+    element.addEventListener('click', expand)
+  })
 }
 
 module.exports = init
