4bc140c06398832890cfc08edea10943acc7b63f
Peter Lyons <pete@peterlyons.com>
2015-10-25 16:41:01 -0600

    WIP browser code for react photos

---
+++ app/browser/main.js
@@ -0,0 +1,14 @@
+// @TODO navigation, carrer, create post, deck, plusParty
+function dispatch (path) {
+  path = path || window.location.pathname
+  switch (path) {
+    case '/photos-react':
+      require('../photosReact/browser-main')
+      break
+    case '':
+      break
+    default:
+  }
+}
+
+dispatch()
--- bin/build_dev_js.sh
+++ bin/build_dev_js.sh
@@ -8,13 +8,7 @@ unset IFS # This screws up our shell quoting
 
 PATH="${PATH}:${PWD}/node_modules/.bin"
 
-# doc "* bundle: generate browser JS for development"
-"${1-browserify}" \
+watchify \
   -o www/plws.js \
   --debug \
-  $(./bin/browserify_args.sh)
-"${1-browserify}" \
-  -o www/plws.js \
-  --debug \
-  $(./bin/browserify_args.sh)
-browserify -o www/reveal.js -e app/browser/deck
+  --entry ./app/browser/main.js
