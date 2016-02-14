6fe2000fff104b2d503c3d26027279102e191d10
Peter Lyons <pete@peterlyons.com>
2016-02-14 14:17:32 -0700

    latest browserify and uglify fixes

--- bin/build_js.sh
+++ bin/build_js.sh
@@ -5,6 +5,7 @@
 cd "$(dirname "$0")/.."
 source ./bin/lib/strict_mode.sh
 unset IFS # This screws up our shell quoting
+export PATH=$PWD/node_modules/.bin:$PATH
 
-browserify $(./bin/browserify_args.sh) "$@" | uglifyjs --no-mangle > www/plws.js
-browserify -e app/browser/deck | uglifyjs --no-mangle > www/reveal.js
+browserify $(./bin/browserify_args.sh) "$@" | uglifyjs --compress > www/plws.js
+browserify -e app/browser/deck | uglifyjs --compress > www/reveal.js
