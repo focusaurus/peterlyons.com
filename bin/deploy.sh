708850b6a43fefb325c473970d21a7064ff56448
Peter Lyons <pete@peterlyons.com>
2016-02-28 12:27:55 -0700

    ansible 2.0 adjustments

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -17,7 +17,6 @@ main() {
   export ANSIBLE_HOST_KEY_CHECKING=False
   ansible-playbook \
     -i "${inventory}" \
-    --ask-sudo-pass \
     --extra-vars "dist=${dist_path}" \
     --extra-vars "dist_name=${dist_name}" \
     ./deploy/playbook-full-stack.yml
