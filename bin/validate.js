5f9bcaa9f3ba5768926e25edd188e15c6597041b
Peter Lyons <pete@peterlyons.com>
2016-03-01 16:56:12 -0700

    new HTML validator package. Fix up.

--- bin/validate.js
+++ bin/validate.js
@@ -1,23 +1,31 @@
 #!/usr/bin/env mocha --reporter spec
 /* eslint-env mocha */
 process.env.NODE_ENV = 'test'
-var request = require('supertest')(require('app'))
-var testConfigs = require('../app/test-configs')
-var w3c = require('w3c-validate').createValidator()
+var htmlValidator = require('html-validator')
 
-describe('The HTML of each page', function () {
-  testConfigs.forEach(function (testConfig) {
-    var URI = testConfig[0]
-    it(URI + ' should be valid HTML5 according to the W3C', function (done) {
-      this.slow(5000)
-      this.timeout(9000)
-      request.get(URI).expect(200).end(function (error, res) {
-        if (error) {
-          done(error)
-          return
-        }
-        w3c.validate(res.text, done)
+function makeTest (request, configs) {
+  describe('The HTML of each page', function () {
+    configs.forEach(function (testConfig) {
+      var URI = testConfig[0]
+      it(URI + ' should be valid HTML5 according to the W3C', function (done) {
+        this.slow(5000)
+        this.timeout(9000)
+        request.get(URI).expect(200).end(function (error, res) {
+          if (error) {
+            done(error)
+            return
+          }
+          var options = {
+            format: 'text',
+            data: res.text
+          }
+          htmlValidator(options, done)
+        })
       })
     })
   })
-})
+}
+
+makeTest(require('../app/request'), require('../app/test-configs'))
+makeTest(
+  require('../app/personal/request'), require('../app/personal/test-configs'))
