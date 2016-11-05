bf8a531438b77867e1b37950bc8e4ff2cee356fd
Peter Lyons <pete@peterlyons.com>
2016-11-05 09:02:51 -0600

    Format eslint as TAP

--- bin/lint.sh
+++ bin/lint.sh
@@ -4,5 +4,5 @@ source ./bin/lib/strict_mode.sh
 export PATH="${PWD}/node_modules/.bin:$PATH"
 
 echo -n "linting…"
-eslint --format ./node_modules/eslint-formatter-comment/index.js app
+eslint --format ./node_modules/eslint-tap app
 echo ✓
