e6875144a1626c507d0cd9573b5ded4f114dbd23
Peter Lyons <pete@peterlyons.com>
2017-04-06 15:49:10 -0600

    Finalize js build script

--- bin/build-js.sh
+++ bin/build-js.sh
@@ -7,26 +7,40 @@ source ./bin/lib/strict_mode.sh
 unset IFS # This screws up our shell quoting
 export PATH="${PWD}/node_modules/.bin:$PATH"
 
-bundler="watchify --debug"
-out="./www/plws.js"
-uglify_args=""
-if [[ "$1" == "production" ]]; then
-  bundler="browserify"
-  uglify_args="--compress warnings=false --mangle --keep-fnames --screw-ie8"
-fi
+build_plus_party() {
+  local plus_party_temp
+  plus_party_temp="$(mktemp -t plus-party-build-XXXX).js"
+  local out="./www/plus-party.js"
+  cd app/plus-party
+  elm-make --yes --output "${plus_party_temp}" PlusParty.elm
+  cd -
+  cat node_modules/clipboard/dist/clipboard.js "${plus_party_temp}" > "${out}"
+  uglifyjs ${uglify_args} "${out}" | gzip > "${out}.gz"
+}
 
-echo -n "${bundler}…"
-${bundler} \
-  --outfile "${out}" \
-  --entry app/browser/navigation \
-  --require app/blog/create-post-main \
-  --require app/pages/career-main \
-  --require app/pages/home-main \
-  --require app/personal/photos/photos-main \
-  --require app/plus-party/plus-party-main
+build_browserify() {
+  local bundler="watchify --debug"
+  local out="./www/plws.js"
+  local uglify_args=""
+  if [[ "$1" == "production" ]]; then
+    bundler="browserify"
+    uglify_args="--compress warnings=false --mangle --keep-fnames --screw-ie8"
+  fi
 
-if [[ "$1" == "production" ]]; then
+  echo -n "${bundler}…"
+  ${bundler} \
+    --outfile "${out}" \
+    --entry app/browser/navigation \
+    --require app/blog/create-post-main \
+    --require app/pages/career-main \
+    --require app/pages/home-main \
+    --require app/personal/photos/photos-main
   temp=$(mktemp -t plws-build-XXXX)
   cp "${out}" "${temp}"
   uglifyjs ${uglify_args} "${temp}" | gzip > "${out}.gz"
+}
+
+if [[ "$1" == "production" ]]; then
+  build_browserify "$@"
+  build_plus_party
 fi
