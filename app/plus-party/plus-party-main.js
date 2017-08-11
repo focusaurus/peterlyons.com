93b80e433af497727fc3e41d13ef593e790aa1cc
Peter Lyons <pete@peterlyons.com>
2017-08-11 16:47:15 -0600

    Latest node and clean up deps

--- app/plus-party/plus-party-main.js
+++
@@ -1,5 +0,0 @@
-const plusParty = require("./plus-party");
-const ReactDOM = require("react-dom");
-
-/* global document */
-ReactDOM.render(plusParty.PlusParty, document.querySelector(".plus-party"));
