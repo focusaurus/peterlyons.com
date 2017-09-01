b5589c79dabea7a830f411131175805c60101126
Peter Lyons <pete@peterlyons.com>
2017-09-01 08:36:47 -0600

    Don't log 404 errors

--- app/errors/error-handler.js
+++ app/errors/error-handler.js
@@ -4,11 +4,11 @@ const log = require("bole")(__filename);
 // eslint-disable-next-line no-unused-vars
 function errorHandler(error, req, res, next) {
   /* eslint no-unused-vars:1 */
-  log.warn(req.path, "app-wide error handler");
   res.status(error.statusCode || 500);
   if (error.statusCode === 404) {
     res.render("errors/error404");
   } else {
+    log.warn(req.path, "app-wide error handler");
     res.render("errors/error500");
     log.error(error, req.path);
   }
