428dbe251fd2cca720a5af907c821a68111680ca
Peter Lyons <pete@peterlyons.com>
2017-11-02 11:46:44 -0600

    Add headless browser tests. Remove travis CI.

--- bin/test-browser.sh
+++ bin/test-browser.sh
@@ -3,14 +3,6 @@
 # run automated test suite and linting
 cd "$(dirname "$0")/.." || exit
 source ./bin/lib/strict_mode.sh
-unset IFS # This screws up groups variable quoting/parsing
-
 echo -n browser tests…
-browser="--phantom"
-if [[ "$1" == "local" ]]; then
-  # use this for real browser
-  browser="--local $(config3 tests.port)"
-fi
-zuul ${browser} --ui mocha-bdd --open \
-  $(find ./app -name '*.btest.js' -print0 | xargs -0)
+node app/headless-tests.js
 echo ✓
