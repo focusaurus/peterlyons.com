c2b4c3c2583bfb913d38bee4e1f6775d1709d059
Peter Lyons <pete@peterlyons.com>
2017-11-19 08:57:47 -0700

    Smaller dev start logging

--- bin/start.sh
+++ bin/start.sh
@@ -3,4 +3,8 @@ cd "$(dirname "$0")/.." || exit
 source ./bin/lib/strict_mode.sh
 export PATH="${PWD}/node_modules/.bin:$PATH"
 
-node-dev --inspect . | tee -a ./log/app.log
+local_dev_format="delete this.v; delete this.hostname;delete this.level; delete this.pid; delete this.name"
+
+node --inspect . \
+  | tee -a ./log/app.log \
+  | json -g -a -0 -e "${local_dev_format}"
