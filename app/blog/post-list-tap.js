ab1391f5f5d551592d2542314dde7362a0719c26
Peter Lyons <pete@peterlyons.com>
2018-03-15 01:25:51 -0600

    Migrate post-list tests from mocha to tap

---
+++ app/blog/post-list-tap.js
@@ -0,0 +1,21 @@
+"use strict";
+const tap = require("tap");
+const testUtils = require("../test-utils");
+
+let server;
+let $;
+
+tap.beforeEach(async () => {
+  server = await require("./test-blog-hapi")();
+  $ = await testUtils.loadDom(server.info.uri, "/utb");
+});
+
+tap.test("blog post list page", test => {
+  test.same($(".postTitle").length, 11, "should list all the posts");
+  test.ok($(".postDate").length > 0);
+  const date = $(".postDate")
+    .last()
+    .html();
+  test.match(date, /December 01, 2015/, "should have nicely formatted dates");
+  test.end();
+});
